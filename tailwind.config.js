4e9dfc80e2cbbaa94100a1337a44fd1a848825de
Hung <hungamtp@gmail.com>
2023-02-22 16:21:38 +0700

    Init commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    screens: {
+      ssm: '20px',
+      sm: '640px',
+      md: '758px',
+      lg: '1024px',
+      xl: '1228px',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
