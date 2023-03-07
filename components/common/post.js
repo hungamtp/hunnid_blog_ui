11632678a1e5ca39dcaa9e04ecebd53ae445cac9
Hung <hungamtp@gmail.com>
2023-03-07 15:33:06 +0700

    fix redirect to page detail

--- components/common/post.js
+++ components/common/post.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 const Post = ({ post, isLoading }) => {
   return (
-    <div className="col-lg-4 col-md-6 mb-4 pb-2">
+    <Link href={`/post/${post.id}`} className="col-lg-4 col-md-6 mb-4 pb-2">
       <div className="card blog rounded border-0 shadow overflow-hidden">
         <div className="position-relative">
           <Image src={'/asset/images/blog/01.jpg'} width={120} height={120} className="card-img-top" alt="..." />
@@ -10,17 +10,15 @@ const Post = ({ post, isLoading }) => {
         </div>
         <div className="card-body content">
           <h5>
-            <Link href={`/post/${post.id}`} className="card-title title text-dark">
-              {post.title}
-            </Link>
+            <div className="card-title title text-dark">{post.title}</div>
           </h5>
           <div className="post-meta d-flex justify-content-between mt-3">
             <ul className="list-unstyled mb-0">
               <li className="list-inline-item">
-                <a href="" className="text-muted eye">
+                <div href="" className="text-muted eye">
                   <i className="uil uil-eye me-1" />
                   {post.content}
-                </a>
+                </div>
               </li>
             </ul>
             <Link href={`post/${post.id}`} className="text-muted readmore animate-waving-hand">
@@ -40,7 +38,7 @@ const Post = ({ post, isLoading }) => {
           </small>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 export default Post;
