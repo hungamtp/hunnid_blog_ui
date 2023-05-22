a217906f4e6a90d3117d18f5a133956ad651a4eb
Hung <hungamtp@gmail.com>
2023-05-22 09:23:31 +0700

    demo slide

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,6 +35,130 @@ const Home = () => {
   return (
     <>
       <div>
+        <section className="home-slider position-relative">
+          <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-inner">
+              <div className="carousel-item active" data-bs-interval={3000}>
+                <div
+                  className="bg-home-75vh d-flex align-items-center"
+                  style={{ background: 'url("/asset/images/blog/01.jpg") center center' }}
+                >
+                  <div className="bg-overlay" />
+                  <div className="container">
+                    <div className="row mt-5 justify-content-center">
+                      <div className="col-12">
+                        <div className="title-heading text-center">
+                          <h2 className="text-white title-dark mb-3">Weekend Travel</h2>
+                          <ul className="list-unstyled">
+                            <li className="list-inline-item small user text-light me-2">
+                              <i className="uil uil-user text-white title-dark" /> Calvin Carlo
+                            </li>
+                            <li className="list-inline-item small date text-light">
+                              <i className="uil uil-calendar-alt text-white title-dark" /> 25th June 2021
+                            </li>
+                          </ul>
+                          <p className="para-desc mx-auto text-white-50 mb-0">
+                            Start working with Landrick that can provide everything you need to generate awareness, drive traffic, connect.
+                          </p>
+                          <div className="mt-4">
+                            <a href="javascript:void(0)" className="btn btn-primary">
+                              Read More{' '}
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                      {/*end col*/}
+                    </div>
+                    {/*end row*/}
+                  </div>
+                </div>
+                {/*end slide*/}
+              </div>
+              <div className="carousel-item" data-bs-interval={3000}>
+                <div
+                  className="bg-home-75vh d-flex align-items-center"
+                  style={{ background: 'url("/asset/images/blog/01.jpg") center center' }}
+                >
+                  <div className="bg-overlay" />
+                  <div className="container">
+                    <div className="row mt-5 justify-content-center">
+                      <div className="col-12">
+                        <div className="title-heading text-center">
+                          <h2 className="text-white title-dark mb-3">Business Meeting</h2>
+                          <ul className="list-unstyled">
+                            <li className="list-inline-item small user text-light me-2">
+                              <i className="uil uil-user text-white title-dark" /> Calvin Carlo
+                            </li>
+                            <li className="list-inline-item small date text-light">
+                              <i className="uil uil-calendar-alt text-white title-dark" /> 25th June 2021
+                            </li>
+                          </ul>
+                          <p className="para-desc mx-auto text-white-50 mb-0">
+                            Start working with Landrick that can provide everything you need to generate awareness, drive traffic, connect.
+                          </p>
+                          <div className="mt-4">
+                            <a href="javascript:void(0)" className="btn btn-primary">
+                              Read More{' '}
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                      {/*end col*/}
+                    </div>
+                    {/*end row*/}
+                  </div>
+                </div>
+                {/*end slide*/}
+              </div>
+              <div className="carousel-item" data-bs-interval={3000}>
+                <div
+                  className="bg-home-75vh d-flex align-items-center"
+                  style={{ background: 'url("/asset/images/blog/01.jpg") center center' }}
+                >
+                  <div className="bg-overlay" />
+                  <div className="container">
+                    <div className="row mt-5 justify-content-center">
+                      <div className="col-12">
+                        <div className="title-heading text-center">
+                          <h2 className="text-white title-dark mb-3">Delicious &amp; Organic</h2>
+                          <ul className="list-unstyled">
+                            <li className="list-inline-item small user text-light me-2">
+                              <i className="uil uil-user text-white title-dark" /> Calvin Carlo
+                            </li>
+                            <li className="list-inline-item small date text-light">
+                              <i className="uil uil-calendar-alt text-white title-dark" /> 25th June 2021
+                            </li>
+                          </ul>
+                          <p className="para-desc mx-auto text-white-50 mb-0">
+                            Start working with Landrick that can provide everything you need to generate awareness, drive traffic, connect.
+                          </p>
+                          <div className="mt-4">
+                            <a href="javascript:void(0)" className="btn btn-primary">
+                              Read More{' '}
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                      {/*end col*/}
+                    </div>
+                    {/*end row*/}
+                  </div>
+                </div>
+                {/*end slide*/}
+              </div>
+            </div>
+            <a className="carousel-control-prev" href="#carouselExampleInterval" role="button" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true" />
+              <span className="visually-hidden">Previous</span>
+            </a>
+            <a className="carousel-control-next" href="#carouselExampleInterval" role="button" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true" />
+              <span className="visually-hidden">Next</span>
+            </a>
+          </div>
+        </section>
+        {/*end section*/}
+
         <section className="section">
           <div className="container">
             <div className="filter-section" style={{ marginBottom: '20px' }}>
